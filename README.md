5f02946fd0fbd47c152e4ae497b48862e94e3e14
Ankur <ankur@Ankurs-iMac.local>
2019-06-07 12:30:25 -0700

    barebones

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # docker-base
+
+## Project contains barebones starter kits for docker 
+It's a first step towards getting up and running in minutes with CI/CD.
+
---
+++ docker-node/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+res.send('Hello World, from docker ==> which is running express')
+})
+
+app.listen(3000, function () {
+console.log('Node is up and running onn docker container cheers 🍺🍺!! 🛥⚓️')
+})
